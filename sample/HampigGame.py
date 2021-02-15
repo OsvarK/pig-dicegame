1a2f579d9d4ff416fb500181839af16d238a533e
Hampus <Haampusandersson@live.se>
2021-02-15 23:17:35 +0100

    Welcome/Exit functions
    Added Welcome/Exit functions

---
+++ sample/HampigGame.py
@@ -0,0 +1,10 @@
+def welcome_message():
+    print("""Welcome to the OlymPigs, the rules are as following:
+          A player can quit his turn whenever he or she wants.
+          If a player rolls a 1, he or she loses all of their accumulated points during the round.
+          Have fun!""")
+
+
+def exit_message():
+    print("""Always remember:
+          The prize is the friends we made along the way.""")
