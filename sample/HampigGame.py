273a94849fddf7fc3fe7c394966a8e0b8336e9c6
Hampus <Haampusandersson@live.se>
2021-02-16 09:09:59 +0100

    Sketch
    Sketching out the basics before bed

--- sample/HampigGame.py
+++ sample/HampigGame.py
@@ -1,10 +1,27 @@
+import random
+
+
 def welcome_message():
     print("""Welcome to the OlymPigs, the rules are as following:
           A player can quit his turn whenever he or she wants.
-          If a player rolls a 1, he or she loses all of their accumulated points during the round.
+          If a player rolls a 1, the player loses all of their accumulated points during that round.
           Have fun!""")
 
 
 def exit_message():
     print("""Always remember:
           The prize is the friends we made along the way.""")
+
+
+class Player:
+    def __init__(self, name, points):
+        self.name = name
+        self.points = points
+
+
+class Scoreboard(self, points):
+    def __init__(self, points):
+        self.points = points
+
+
+def player1
