cb0d654ad96db8c01892071c6f3cbcb1d313a9c6
Oscar Karlsson <oscar.karl00@hotmail.com>
2021-02-18 17:30:52 +0100

    Added unit testing for game.py

--- src/game.py
+++ src/game.py
@@ -1,4 +1,4 @@
-from userInterface import UI
+from src.userInterface import UserInterface
 
 
 class Game:
@@ -22,46 +22,59 @@ class Game:
     def __init__(self, players):
         """ Constructs the necessary logic needed to run the game """
         self.__players = players
-        self.__ui = UI()
+        self.__ui = UserInterface()
         self.__gameIsActive = True
+
+    def startGame(self):
         self.__gameLoop()
 
     def __gameLoop(self):
-        """ This fucntion controlls the flow of the game (game loop) """
+        """ This fucntion controlls the flow of the game (the game loop) """
         turnIndex = 0
-        # Turn loop
         while(self.__gameIsActive):
-            # init this turn
-            player = self.__players[turnIndex]
-            firstThrow = True
-            points = 0
-            # Throw dice loop
-            while(True):
-                # Throw dice
-                dice = self.__ui.throwDiceQuestion(firstThrow)
-                firstThrow = False
-                if dice == -1:
-                    # No dice thrown
-                    break
-                elif dice == 1:
-                    # Dice equals 1, remove all points & end turn.
-                    points = 0
-                    break
-                else:
-                    # Append dice number to points pool.
-                    points += dice
-            # Add score to player
-            player.addScore(points)
-            # Check if the player has won
-            if player.getScore() >= 100:
-                self.__GameOver(player)
-            # Get new turn index
-            if turnIndex == len(self.__players):
-                turnIndex = 0
-            else:
-                turnIndex += 1
+            player = self.__players[turnIndex]      # Refreance current player.
+            points = self.diceThrowLoop()           # player throw dice.
+            player.addScore(points)                 # add score to player.
+            turnIndex = self.updateTurnIndex()      # update turn index.
+            if self.hasPlayerWon(player):         # check if player has won.
+                self.GameOver(player)
+                break
 
-    def __GameOver(self, player):
+    def diceThrowLoop(self):
+        """
+        This function controlls the flow of the dice loop,
+        the players ability to throw dices.
+        """
+        firstThrow = True
+        points = 0
+        while(True):
+            dice = self.__ui.throwDiceQuestion(firstThrow)
+            firstThrow = False
+            if dice == -1:
+                # No dice thrown
+                break
+            elif dice == 1:
+                # Dice equals 1, remove all points & end turn.
+                points = 0
+                break
+            else:
+                # Append dice number to points pool.
+                points += dice
+        return points
+
+    def hasPlayerWon(self, player):
+        """ Checks if player has >= 100 points """
+        return player.getScore() >= 100
+
+    def updateTurnIndex(self, turnIndex):
+        """ Updates the turn index, to keep track on who's turn it is """
+        if turnIndex == (len(self.__players) - 1):
+            turnIndex = 0
+        else:
+            turnIndex += 1
+        return turnIndex
+
+    def GameOver(self, player):
         """ Method that handels logic on game over"""
         self.__gameIsActive = False
         pass
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,29 @@
 import unittest
 from src.game import Game
+from src.player import Player
 
 
 class TestGame(unittest.TestCase):
-    pass
+
+    def test_hasPlayerWon(self):
+        """Tests game:hasPlayerWon function"""
+        game = Game({1, 2, 3, 4})
+        player = Player()
+        self.assertFalse(game.hasPlayerWon(player))
+        player.addScore(99)
+        self.assertFalse(game.hasPlayerWon(player))
+        player.addScore(1)
+        self.assertTrue(game.hasPlayerWon(player))
+
+    def test_updateTurnIndex(self):
+        """Tests game:updateTurnIndex function"""
+        game = Game({1, 2, 3, 4})
+        self.assertEqual(game.updateTurnIndex(0), 1)
+        self.assertEqual(game.updateTurnIndex(1), 2)
+        self.assertEqual(game.updateTurnIndex(2), 3)
+        self.assertEqual(game.updateTurnIndex(3), 0)
+
+
+# Run unitest, can also be runnned by 'py -m unititest test_game.py'
+if __name__ == '__main__':
+    unittest.main()
