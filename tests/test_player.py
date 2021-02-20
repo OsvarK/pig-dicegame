c58486d75a21fe3799fad176ed9b7896931d85e7
Oscar Karlsson <oscar.karl00@hotmail.com>
2021-02-20 11:40:32 +0100

    Added test for throw dice

--- tests/test_player.py
+++ tests/test_player.py
@@ -3,7 +3,12 @@ from src.player import Player
 
 
 class TestPlayer(unittest.TestCase):
-    pass
+
+    def test_throwDice(self):
+        """Tests Player:throwDice"""
+        player = Player("Tester")
+        for i in range(1, 20):
+            self.assertTrue(player.throwDice() in range(1, 6))
 
 
 # Run unitest, can also be runnned by 'py -m unititest test_player.py'
