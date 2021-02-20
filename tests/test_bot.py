997fc8f09e2a6adbe1c411bcdafb8d8d5c52685e
Oscar Karlsson <oscar.karl00@hotmail.com>
2021-02-20 11:31:08 +0100

    update

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -3,12 +3,19 @@ from src.bot import Bot
 
 
 class TestBot(unittest.TestCase):
-    def calculateHowManyThrows(self):
-        """Tests bbot:calculateHowManyThrows function"""
+
+    def test_calculateHowManyThrows(self):
+        """Tests bot:calculateHowManyThrows function"""
         bot = Bot(None)
         for x in range(1, 20):
             y = bot.calculateHowManyThrows()
             self.assertTrue(y >= 1)
+
+    def test_getDiceThrows(self):
+        """Tests bot:getDiceThrows function"""
+        b = Bot()
+        for i in range(0, 20):
+            self.assertTrue(b.getDiceThrows(None) > 1)
 
 
 # Run unitest, can also be runnned by 'py -m unititest test_boy.py'
