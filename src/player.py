888b3f8ec3308be609dcc381d0e31aa31c15140a
Oscar Karlsson <oscar.karl00@hotmail.com>
2021-02-20 12:17:23 +0100

    boiler in player

--- src/player.py
+++ src/player.py
@@ -19,6 +19,30 @@ class Player(object):
         super.__init__()
         self.score = 0
         self.username = username
+        self.wins = 0
+        self.higestScoreInOneTurn = []
+        self.fastestWin = []
 
     def throwDice(self):
+        """Throws the dice"""
         return random.Randomint(1, 6)
+
+    def fetchData(self):
+        """Fetch data from storage"""
+        self.wins = 0
+        self.higestScoreInOneTurn = []
+        self.fastestWin = []
+        pass
+
+    def ishigestScoreInOneTurn(self, newHigescore):
+        """Check if this is the new higescore in one turn"""
+        pass
+
+    def isfastestWin(self, onTurn):
+        """Check if this is the fastes win"""
+        pass
+
+    def playerWon(self, onTurn):
+        """This player won the game"""
+        self.win += 1
+        self.isfastestWin(onTurn)
