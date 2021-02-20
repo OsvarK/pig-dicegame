367b11763442b7705800f91e2309dd8e59cb1a83
Oscar Karlsson <oscar.karl00@hotmail.com>
2021-02-20 17:41:29 +0100

    removed func from testUserInterface

--- tests/test_userInterface.py
+++ tests/test_userInterface.py
@@ -1,16 +1,8 @@
 import unittest
-from pigdicegame.userInterface import UserInterface
-from pigdicegame.player import Player
 
 
 class TestUserInterface(unittest.TestCase):
-
-    def throwDiceLoop(self):
-        """ Tests UserInterface:throwDiceLoop"""
-        ui = UserInterface()
-        player = Player()
-        result = ui.throwDiceLoop(player)
-        self.assertTrue(result in range(-1, 6))
+    pass
 
 
 # Run by typing: py <filename>
