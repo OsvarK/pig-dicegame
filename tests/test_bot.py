605d25342ec0409720b8bab2c4e9759b00651151
Oscar Karlsson <oscar.karl00@hotmail.com>
2021-02-24 11:29:54 +0100

    bot() -> bot(None)

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -11,19 +11,19 @@ class TestBot(unittest.TestCase):
 
     def test_calculateHowManyThrows(self):
         """Tests bot:calculateHowManyThrows function"""
-        bot = Bot()
+        bot = Bot(None)
         for x in range(1, 200):
             y = bot.calculateHowManyThrows()
             self.assertTrue(y >= 1)
 
     def test_getDiceThrows(self):
         """Tests bot:getDiceThrows function"""
-        b = Bot()
+        b = Bot(None)
         for i in range(0, 20):
             self.assertTrue(b.getDiceThrows() >= 0)
 
     def test_wantToBank(self):
-        b = Bot()
+        b = Bot(None)
         self.assertTrue(b.wantToBank(12, 2))
         self.assertTrue(b.wantToBank(13, 4))
         self.assertTrue(b.wantToBank(7, 2))
@@ -33,7 +33,7 @@ class TestBot(unittest.TestCase):
 
     def test_getRandomBotName(self):
         """Tests bot:getRandomBotName function"""
-        b = Bot()
+        b = Bot(None)
         PATH = os.path.dirname(os.path.realpath(__file__))
         with self.assertRaises(FileNotFoundError):
             b.getRandomBotName(PATH + "\\resources\\namebotsss.txt")
