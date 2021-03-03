515bcde42af7c64426bc8b00ccd1542150d8bdd5
Stefan Claesson <stefan.claesson0001@stud.hkr.se>
2021-03-03 13:24:43 +0100

    Resolved problems with snake-case and trailing whitespaces, added doc strings

--- pigdicegame/lib/highscore.py
+++ pigdicegame/lib/highscore.py
@@ -1,21 +1,31 @@
+"""
+Module to handle everything connected to data for highscores
+"""
+
 import json
 from player import Player
 
 class Highscore():
+    """
+    Class represents a the data for highscore and list of players.
+    """
 
     def __init__(self):
         self.highscore = []
         self.players = []
 
 
-    def createHighScore(self):
+    def create_highscore(self):
+        """ Converts the list of player-object to JSON format and calls to save the data.
+            Needs to be called before the game is closed.
+        """
         new_score = []
         for player in self.players:
             new_score.append(player.__dict__)
         self.highscore = new_score
-        self.saveData()
+        self.save_data()
 
-    def saveData(self):
+    def save_data(self):
         """Save data to storage"""
         try:
             with open("LocalHighscore.json", "w") as file:
@@ -23,8 +33,8 @@ class Highscore():
         except FileNotFoundError:
             print("Error: File Not Found")
 
-            
-    def loadData(self):
+
+    def load_data(self):
         """Fetch data from storage"""
         try:
             with open("LocalHighscore.json", "r") as file:
@@ -33,7 +43,11 @@ class Highscore():
             print("Error: File not found")
 
 
-    def createPlayers(self):
+    def create_players(self):
+        """ Creates players from the loaded data.
+            Needs to be called as the game starts
+        """
+        self.load_data()
         for entry in self.highscore:
             player = Player(entry["username"])
             player.score = entry["score"]
