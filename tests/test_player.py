b782d7ac84c535a8d5d2ac49487955a9464c3e95
Oscar Karlsson <oscar.karl00@hotmail.com>
2021-03-03 09:46:09 +0100

    Added test_isFastestWin:test:player.py

--- tests/test_player.py
+++ tests/test_player.py
@@ -14,3 +14,25 @@ class TestPlayer(unittest.TestCase):
         for i in range(1, 20):
             self.assertTrue(player.throwDice() in range(0, 7))
 
+    def test_loadDate(self):
+        pass
+
+    def test_saveData(self):
+        pass
+
+    def test_ishigestScoreInOneTurn(self):
+        pass
+
+    def test_isfastestWin(self):
+        player = Player("Tester")
+        player.fastestWin = 10
+        player.isfastestWin(12)
+        self.assertFalse(player.fastestWin == 12)
+        self.assertTrue(player.fastestWin == 10)
+        player.isfastestWin(5)
+        self.assertFalse(player.fastestWin == 10)
+        self.assertTrue(player.fastestWin == 5)
+        pass
+
+    def test_iWin(self):
+        pass
