0916c016c79cace12eb1ee082f21e240a5ae4ebd
Stefan Claesson <stefan.claesson0001@stud.hkr.se>
2021-03-03 12:07:12 +0100

    Added function to create the highscore from the players in the list of players

--- pigdicegame/lib/highscore.py
+++ pigdicegame/lib/highscore.py
@@ -5,8 +5,24 @@ class Highscore():
 
     def __init__(self):
         self.highscore = []
-        self.player = []
+        self.players = []
 
+    def createHighScore(self):
+        new_score = []
+        for player in self.players:
+            new_score.append(player.__dict__)
+        self.highscore = new_score
+        self.saveData()
+
+    def saveData(self):
+        """Save data to storage"""
+        try:
+            with open("LocalHighscore.json", "w") as file:
+                json.dump(self.highscore, file, indent=2)
+        except FileNotFoundError:
+            print("Error: File Not Found")
+
+            
     def loadData(self):
         """Fetch data from storage"""
         try:
@@ -14,13 +30,3 @@ class Highscore():
                 self.highscore = json.load(file)
         except FileNotFoundError:
             print("Error: File not found")
-
-    def saveData(self):
-        print("saveData")
-        """Save data to storage"""
-        # TODO: save data to storage
-        try:
-            with open("LocalHighscore.json", "w") as file:
-                json.dump(self.highscore, file, indent=2)
-        except FileNotFoundError:
-            print("Error: File Not Found")
