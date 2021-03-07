0e9f7212114f997c6fb62d277f949d705b116711
Oscar Karlsson <oscar.karl00@hotmail.com>
2021-03-07 11:23:09 +0100

    Fixed small pep8 in higescore

--- pigdicegame/lib/highscore.py
+++ pigdicegame/lib/highscore.py
@@ -5,6 +5,7 @@ Module to handle everything connected to data for highscores
 import json
 from player import Player
 
+
 class Highscore():
     """
     Class represents a the data for highscore and list of players.
@@ -14,10 +15,10 @@ class Highscore():
         self.highscore = []
         self.players = []
 
-
     def create_highscore(self):
-        """ Converts the list of player-object to JSON format and calls to save the data.
-            Needs to be called before the game is closed.
+        """ Converts the list of player-object to JSON
+        format and calls to save the data.
+        Needs to be called before the game is closed.
         """
         new_score = []
         for player in self.players:
@@ -28,20 +29,18 @@ class Highscore():
     def save_data(self):
         """Save data to storage"""
         try:
-            with open("pigdicegame/resources/LocalHighscore.json", "w") as file:
+            with open("pigdicegame/resources/LocalHighscore.json", "w")as file:
                 json.dump(self.highscore, file, indent=2)
         except FileNotFoundError:
             print("Error: File Not Found")
 
-
     def load_data(self):
         """Fetch data from storage"""
         try:
-            with open("pigdicegame/resources/LocalHighscore.json", "r") as file:
+            with open("pigdicegame/resources/LocalHighscore.json", "r")as file:
                 self.highscore = json.load(file)
         except FileNotFoundError:
             print("Error: File not found")
-
 
     def create_players(self):
         """ Creates players from the loaded data.
